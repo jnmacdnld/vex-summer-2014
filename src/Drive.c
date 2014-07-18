f103c07a53dd9abfaf3c94e109f42d290921db5e
John Macdonald <jnmacdnld@gmail.com>
2014-07-18 13:51:29 -0400

    implement DriveInit to initialize pid controllers

--- src/Drive.c
+++ src/Drive.c
@@ -2,9 +2,18 @@
 
 #include "main.h"
 #include "Motors.h"
+#include "Pid.h"
+
+#define MOVE_STRAIGHT_TICKS_P_GAIN 1.0
+#define PIVOT_TICKS_P_GAIN 1.0
+
+static PidController* move_straight_ticks_pid;
+static PidController* pivot_ticks_pid;
 
 void DriveInit() {
-  
+  move_straight_ticks_pid = PidInitController(MOVE_STRAIGHT_TICKS_P_GAIN, 0.0,
+    0.0);
+  pivot_ticks_pid = PidInitController(PIVOT_TICKS_P_GAIN, 0.0, 0.0);
 }
 
 void DriveSetLeftPower(short setting) {
@@ -14,10 +23,9 @@ void DriveSetLeftPower(short setting) {
 }
 
 void DriveSetRightPower(short setting) {
-  MotorsSetAdjusted(FRONT_RIGHT_DRIVE, setting);
-  MotorsSetAdjusted(MIDDLE_RIGHT_DRIVE, setting);
-  MotorsSetAdjusted(BACK_RIGHT_DRIVE, setting);
-
+  MotorsSetAdjusted(FRONT_RIGHT_DRIVE, -setting);
+  MotorsSetAdjusted(MIDDLE_RIGHT_DRIVE, -setting);
+  MotorsSetAdjusted(BACK_RIGHT_DRIVE, -setting);
 }
 
 void DriveSetPower(short setting) {
